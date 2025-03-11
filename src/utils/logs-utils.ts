28f642eebaab5fabf12365fae986739f599d16e3
Alexander Tejeda <AlexanderTejedaBarahona10@gmail.com>
2025-03-10 23:02:19 -0600

    Modificacion de servicio para guardado de logs

--- src/utils/logs-utils.ts
+++ src/utils/logs-utils.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import logger from "../config/logger"
+import { NextFunction } from "express";
 
 const prisma = new PrismaClient(); 
 
-export const saveLogs = () => {
+export const saveLogs = (req : Request, res : Response, next : NextFunction) => {
 
     logger.info("Saving information in logs"); 
+    console.log(req.body); 
 
 }
