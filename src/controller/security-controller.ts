3824cd6ff8320451057aa1617dc072c3f6b9f384
Alexander Ismael Tejeda Bara <aitejeda@bancatlan.hn>
2025-03-25 20:02:20 -0600

    Feature: Modificacion de servicio para agregar el envio por correo al cliente que solicito el API-KEY

--- src/controller/security-controller.ts
+++ src/controller/security-controller.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express"
 
 export const generateApiKey = async (req : Request, res : Response, next : NextFunction) => {
-    console.log("Se esta generando el API KEY")
+    
     res.status(200)
     .send({
         "time" : "today", 
         "email" : "Alexander"
     });
+
 }
