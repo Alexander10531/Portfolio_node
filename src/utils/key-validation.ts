8f4fd542b281d8f947a518b685fd570398a48340
Alexander Ismael Tejeda Bara <aitejeda@bancatlan.hn>
2025-05-08 14:00:20 -0600

    Feature (Version no estable) Se esta modificando el servicio dado que se esta validando las credenciales enviadas en el header, sin embargo al momento de generar la excepcion este sigue ejecutando el controlador.

--- src/utils/key-validation.ts
+++ src/utils/key-validation.ts
@@ -1,40 +1,58 @@
+import { PrismaClient } from "@prisma/client";
 import crypto from "crypto";
 import { NextFunction, Request, Response } from "express";
+import CustomException from "../classes/custom-error";
 
-export const validarFirma = (req : Request, res : Response, next : NextFunction) => {
+const prisma = new PrismaClient();
 
-    console.log("----------------"); 
-    console.log(req.headers['public-key']);
-    const publicKeyBase64 = '9Us6XwKAUPJ9XppgDTQBWdU2Ax/TqTWwQyutaHVyqRo=';
-    const requestId = 'e6cc6b91-6faa-4f27-a567-db55f21232da';
-    const signatureBase64 = 'koDxk0ZWQyzFdStMF8nnPl30mzCKSAlVIuBhWt/ezmueWUo67DCHp44SYJhzHP1rc7EEDX4d+PtuGb0YDEKoAw==';
+export const validarFirma = (req: Request, res: Response, next: NextFunction) => {
 
-    const rawPublicKey = Buffer.from(publicKeyBase64, 'base64');
-    const signature = Buffer.from(signatureBase64, 'base64');
+    const publicKeyBase64: String = String(req.headers['public-key']);
+    const requestId: String = String(req.headers['request-id']);
+    const signatureBase64: String = String(req.headers['signature']);
+    let isValid: boolean = true;
 
-    const spkiPrefix = Buffer.from([
-        0x30, 0x2a,
-        0x30, 0x05,
-        0x06, 0x03, 0x2b, 0x65, 0x70,
-        0x03, 0x21, 0x00
-      ]);
-      
-      const spkiKey = Buffer.concat([spkiPrefix, rawPublicKey]);
+    try{
+    
+        const rawPublicKey = Buffer.from(publicKeyBase64, 'base64');
+        const signature = Buffer.from(signatureBase64, 'base64');
+    
+        const spkiPrefix = Buffer.from([
+            0x30, 0x2a,
+            0x30, 0x05,
+            0x06, 0x03, 0x2b, 0x65, 0x70,
+            0x03, 0x21, 0x00
+        ]);
+    
+        const spkiKey = Buffer.concat([spkiPrefix, rawPublicKey]);
+    
+        const keyObject = crypto.createPublicKey({
+            key: spkiKey,
+            format: 'der',
+            type: 'spki',
+        });
+    
+        isValid = crypto.verify(
+            null,
+            Buffer.from(requestId),
+            keyObject,
+            signature
+        );
 
-      const keyObject = crypto.createPublicKey({
-        key: spkiKey,
-        format: 'der',
-        type: 'spki',
-      });
+        if(!isValid){
+            isValid = false;
+        }
+        next();
+    }catch(e){
+        if(!!(publicKeyBase64 || requestId || signatureBase64)){
+            throw new CustomException("No se estan enviando los headers necesarios", 200, []);
+        }
+        throw new CustomException("Se genero un error no controlado al validar la firma", 200, []);
+    }
 
-      const isValid = crypto.verify(
-        null, 
-        Buffer.from(requestId), 
-        keyObject,
-        signature
-      );
+    if(!isValid){
+        throw new CustomException("Credenciales no validas", 200, []);
+    }
 
-      console.log("isValid: ", isValid);
-      next(); 
 
 }
